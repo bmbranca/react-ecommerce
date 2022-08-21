629cf18866bb232ee00c2a40fd6374ec5a1bd919
Brian Branca <brianbranca16@gmail.com>
2022-08-21 10:43:50 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from "react"
-import {Routes, Route} from 'react-router-dom'
-import Header from "./components/Header"
-import Cart from "./pages/Cart"
-import Photos from "./pages/Photos"
-import Styles from "./styles.css"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Cart from "./pages/Cart";
+import Photos from "./pages/Photos";
+import Styles from "./styles.css";
 
-
-function App() {    
+function App() {
   return (
-      <div>
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Photos />}>
-            </Route>
-            <Route path="/cart" element={<Cart />}>
-            </Route>           
-          </Routes>
-
-      </div>
-  )
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Photos />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Context.js
+++ src/Context.js
@@ -1,70 +1,53 @@
-import React, {useState, useEffect} from "react"
-import imagesData from "./imagesData"
+import React, { useState, useEffect } from "react";
+import imagesData from "./imagesData";
 
-const Context = React.createContext()
+const Context = React.createContext();
 
-function ContextProvider({children}) {
-    const [allPhotos, setAllPhotos] = useState([])
-    const [cartItems, setCartItems] = useState([])
-    
-    // const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
-    useEffect(() => {
-        setAllPhotos(imagesData)
-    }, [])
+function ContextProvider({ children }) {
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    // setAllPhotos(imagesData)
-    
-    function toggleFavorite(id) {
-        const updatedArr = allPhotos.map(photo => {
-            if(photo.id === id) {
-                return {...photo, isFavorite: !photo.isFavorite}
-            }
-            return photo
-        })
-        
-        setAllPhotos(updatedArr)
-    }
-    
-    function addToCart(newItem) {
-        setCartItems(prevItems => [...prevItems, newItem])
-    }
-    
-    function removeFromCart(id) {
-        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
-    }
-    
-    function emptyCart() {
-        setCartItems([])
-    }
-    
-    return (
-        <Context.Provider value={{
-            allPhotos, 
-            toggleFavorite, 
-            cartItems, 
-            addToCart, 
-            removeFromCart, 
-            emptyCart
-        }}>
-            {children}
-        </Context.Provider>
-    )
+  useEffect(() => {
+    setAllPhotos(imagesData);
+  }, []);
+
+  function toggleFavorite(id) {
+    const updatedArr = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return { ...photo, isFavorite: !photo.isFavorite };
+      }
+      return photo;
+    });
+
+    setAllPhotos(updatedArr);
+  }
+
+  function addToCart(newItem) {
+    setCartItems((prevItems) => [...prevItems, newItem]);
+  }
+
+  function removeFromCart(id) {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function emptyCart() {
+    setCartItems([]);
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        toggleFavorite,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        emptyCart,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
 
-export {ContextProvider, Context}
-
-
-
-// export default {
-//     "success": true,
-//     "data": {
-//         "memes": [
-//             {
-//                 "id": "181913649",
-//                 "name": "Drake Hotline Bling",
-//                 "url": "https://i.imgflip.com/30b1gx.jpg",
-//                 "width": 1200,
-//                 "height": 1200,
-//                 "box_count": 2
-//             },
-            
+export { ContextProvider, Context };
