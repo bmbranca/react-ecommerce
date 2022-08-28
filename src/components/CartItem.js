e7ca19fdf1b9e53102491965d2616b42ebb1c9d4
Brian Branca <brianbranca16@gmail.com>
2022-08-27 19:04:56 -0500

    changing styling on checkout page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,18 +1,19 @@
-import React, {useState, useContext} from "react"
-import {Context} from "../Context"
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
 
-function CartItem({item}) {
-    const {removeFromCart} = useContext(Context)
+function CartItem({ item }) {
+  const { removeFromCart } = useContext(Context);
 
-    return (
-        <div className="cart-item">
-            <i 
-            className="ri-delete-bin-line" 
-            onClick={() => removeFromCart(item.id)}></i>
-            <img src={item.url} width="130px" />
-            <p>$5.99</p>
-        </div>
-    )
+  return (
+    <div className="cart-item">
+      <i
+        className="ri-delete-bin-line"
+        onClick={() => removeFromCart(item.id)}
+      ></i>
+      <img src={item.url} width="130px" />
+      <p>{item.price}</p>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
